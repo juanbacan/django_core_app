5d4f96cc235efec08e06ab29827987652f68fb28
juaninga <juan.inga@unae.edu.ec>
2025-03-03 20:54:01 -0500

    [FIX] fixed  the app name

--- django_core_app/apps.py
+++ django_core_app/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    name = 'django_core_app'
 
     def ready(self):
         import django_core_app.signals
