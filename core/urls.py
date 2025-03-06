5eedc31e8432a91a63b18d6bf82f78e59a969ec0
juaninga <juan.inga@unae.edu.ec>
2025-03-05 19:12:12 -0500

    [FIX] Urls of third party

--- core/urls.py
+++ core/urls.py
@@ -9,11 +9,4 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('webpush/', include('webpush.urls')),
     path('', include('pwa.urls')),
-
-    # Custom apps
-    path('custom_user_autocomplete/', CustomUserAutocompleteView.as_view(), name='custom_user_autocomplete'),
-    path('model_autocomplete/', ModelAutocompleteView.as_view(), name='model_autocomplete'),
-    path('loginModal/', LoginModalView.as_view(), name='loginModal'),
-    path('upload_image/', upload_image, name='upload_image'),
-    path('api/', api, name='api_administracion'), 
 ]
