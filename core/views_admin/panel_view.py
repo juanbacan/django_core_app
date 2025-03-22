97429d15d98aafd9db46bd2e53e66cee05507209
juaninga <juan.inga@unae.edu.ec>
2025-03-22 17:36:43 -0500

    [FIX] View

--- core/views_admin/panel_view.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from core.views_admin import ViewAdministracionBase
-from administracion_forms import *
-
-class PanelView(ViewAdministracionBase):
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        return render(request, 'administracion/administracion.html', context)
-
